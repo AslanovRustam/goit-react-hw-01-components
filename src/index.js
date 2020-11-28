a4da32e3ce8100e27053c027949a07c073591490
Aslanov Rustam <aslanovrg@gmail.com>
2020-11-28 16:03:20 +0200

    добавляет propptypes  компонентам

--- src/index.js
+++ src/index.js
@@ -1,50 +1,19 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import user from './user.json';
+import App from './App';
 
-function Profile(props) {
-  const { src, alt, name, tag, location, followers, views, likes } = props;
-  return (
-       <div class="profile">
-  <div class="description">
-    <img
-      src={props.src}
-      alt={props.alt}
-      class="avatar"
-    />
-    <p class="name">{props.name}</p>
-    <p class="tag">{props.tag}</p>
-    <p class="location">{props.location}</p>
-  </div>
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">{props.followers}</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">{props.views}</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">{props.likes}</span>
-    </li>
-  </ul>
-</div>
-  )
-  
-};
+
+
 
 ReactDOM.render(
-  <Profile
-    src={user.avatar}
-    alt={user.name}
-    name={user.name}
-    tag={user.tag}
-    location={user.location}
-    followers={user.stats.followers}
-    views={user.stats.views}
-    likes={user.stats.likes} />,
+  <App />,
   document.querySelector('#root'));
 
 
